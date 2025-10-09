75c93460801f0d087299589b83616827d1ca8405
Fabiovgc <fabiovgc@gmail.com>
2025-10-09 19:38:13 -0300

    feat aad : something

--- src/AgendaPro.Api/Wrappers/ApiResponse.cs
+++ src/AgendaPro.Api/Wrappers/ApiResponse.cs
@@ -1,6 +1,26 @@
 ﻿namespace AgendaPro.Api.Wrappers
 {
-    public class ApiResponde
+    public class ApiResponse<T>
     {
+        public bool Success { get; set; }
+        public T Data { get; set; }
+        public List<string>? Errors { get; set; }
+        public string TraceId { get; set; }
+
+        public ApiResponse(T data)
+        {
+            Success = true;
+            Data = data;
+            TraceId = Guid.NewGuid().ToString();
+        }
+
+        public ApiResponse(List<string> errors)
+        {
+            Success = false;
+            Errors = errors;
+            TraceId = Guid.NewGuid().ToString();
+        }
     }
+
+
 }
