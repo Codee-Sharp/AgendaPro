69466f7b389810c96be812faa5146f5809d38791
Fabiovgc <fabiovgc@gmail.com>
2025-10-27 14:17:11 -0300

    fix: moving 'Repositories' folder from 'Tags' folder to 'Service' folder

--- src/AgendaPro.Domain/Services/Repositories/IServiceRepository.cs
+++ src/AgendaPro.Domain/Services/Repositories/IServiceRepository.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AgendaPro.Domain.Tags.Repositories
+namespace AgendaPro.Domain.Services.Repositories
 {
     public interface IServiceRepository
     {
