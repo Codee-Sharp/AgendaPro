646f1067c0a28f6640892802e5a3459ee85027c3
Fabiovgc <fabiovgc@gmail.com>
2025-11-28 14:50:34 -0300

    Creating Entity 'Client'

---
+++ src/AgendaPro.Api/Controllers/ClientsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgendaPro.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ src/AgendaPro.Domain/Clients/Models/ClientModel.cs
@@ -0,0 +1,48 @@
+﻿using AgendaPro.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgendaPro.Domain.Clients.Models
+{
+    internal class ClientModel : AuditableEntity
+    {
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string? Email { get; private set; }
+        public string? Telephone { get; private set; }
+        public string? Observations { get; private set; }
+
+        public ClientModel(Guid createdBy, string name, string? email, string? telephone, string? observations)
+            : base(createdBy)
+        {
+            SetName(name);
+            
+            Email = email;
+            Telephone = telephone;
+            Observations = observations;
+        
+        }
+
+        public void Update(string name, string? email, string? telephone, string? observations)
+        {
+            SetName(name);
+            Email = email;
+            Telephone = telephone;
+            Observations = observations;
+        }
+
+        public void SetName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Nome é obrigatório.", nameof(name));
+            }
+            Name = name;
+        }
+
+
+    }
+}
