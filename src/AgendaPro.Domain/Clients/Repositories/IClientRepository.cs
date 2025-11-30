d8185d83635228f2ed910256079277d5849adab6
Fabiovgc <fabiovgc@gmail.com>
2025-11-29 21:25:03 -0300

    Creating IClientRepository

---
+++ src/AgendaPro.Domain/Clients/Repositories/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaPro.Domain.Clients.Repositories
+{
+    internal interface IClientRepository
+    {
+    }
+}
