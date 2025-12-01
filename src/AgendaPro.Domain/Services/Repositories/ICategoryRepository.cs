64a4915f16a0b6ebd17e0b2d492273a1f6f48c0a
Everton Ferreira <iseverton2024@gmail.com>
2025-12-01 20:30:13 -0300

    feat(infra): implement category repository

---
+++ src/AgendaPro.Domain/Services/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using AgendaPro.Domain.Services.Models;
+
+namespace AgendaPro.Domain.Services.Repositories;
+
+public interface ICategoryRepository
+{
+   Task<IEnumerable<CategoryModel>> GetAllAsync(CancellationToken cancellationToken);
+    Task<CategoryModel?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task<CategoryModel?> GetByNameAsync(string name, CancellationToken cancellationToken);
+    void Add(CategoryModel model);
+    void Update(CategoryModel model);
+    void Delete(CategoryModel model);
+    Task SaveChangesAsync(CancellationToken cancellationToken);
+}
