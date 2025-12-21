7ec304518ed558c00a880270a006703bcc6296b6
Fabiovgc <fabiovgc@gmail.com>
2025-12-21 19:20:23 -0300

    creating clientDTO

---
+++ src/AgendaPro.Application/Clients/DTOs/ClientDTO.cs
@@ -0,0 +1,46 @@
+﻿using AgendaPro.Domain.Clients.Models;
+using AgendaPro.Domain.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AgendaPro.Application.Clients.DTOs
+{
+    public class ClientDTO
+    {
+        public const int minimumCharactersForNumber = 8;
+        
+        public const int maximumCharactersForNumber = 16;
+
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Email { get; set; }
+
+        [StringLength(maximumCharactersForNumber, MinimumLength = minimumCharactersForNumber, 
+            ErrorMessage = "O telefone deve ter entre 8 e 15 caracteres")]
+        [RegularExpression(@"^[0-9+\-\s()]{8,15}$", 
+            ErrorMessage = "O telefone contém caracteres inválidos.")]
+        public string? Telephone { get; set; }
+
+        public string? Observations { get; set; }
+
+
+        public ClientDTO() { }
+
+        public ClientDTO(ClientModel model)
+        {
+
+            Id = model.Id;
+            Name = model.Name;
+            Email = model.Email;
+            Telephone = model.Telephone;
+            Observations = model.Observations;
+
+        }
+
+    }
+}
---
+++ src/AgendaPro.Application/Clients/UseCases/ClientUseCase.cs
@@ -0,0 +1,48 @@
+﻿using AgendaPro.Application.Clients.DTOs;
+using AgendaPro.Domain.Clients.Models;
+using AgendaPro.Domain.Clients.Repositories;
+using AgendaPro.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AgendaPro.Application.Clients.UseCases
+{
+    internal class ClientUseCase
+    {
+
+        private readonly IClientRepository _clientRepository;
+
+        public ClientUseCase(IClientRepository clientRepository)
+        {
+
+               _clientRepository = clientRepository;
+        
+        }
+
+
+        public async Task<Result<ClientDTO>> CreateAsync(ClientDTO clientDTO)
+        {
+            var clientId = Guid.Empty;
+
+            var model = new ClientModel
+            (
+                clientDTO.Name,
+                clientDTO.Email,
+                clientDTO.Telephone,
+                clientDTO.Observations,
+                clienId
+            );
+
+            await _clientRepository.SaveAsync(model);
+
+            var response = new ClientDTO(model);
+
+            return Result<ClientDTO>.Success(response);
+
+        }
+
+
+    }
+}
